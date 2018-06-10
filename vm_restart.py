53c4ab072f5e99609eb0c591560c1295a0450e1e
Timbaland <422033564@qq.com>
2018-06-10 21:36:19 +0800

    upade ssh bug preoblem

--- vm_restart.py
+++ vm_restart.py
@@ -6,9 +6,13 @@
 # from dateutil import parser
 import MySQLdb,sys, os,time,datetime,re
 from vm_tool import connect,exec_commands
+from vm_tool import Logger
+import logging
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 # 连接mysql数据库参数字段
 con = None
-ip = '172.25.1.13'
+ip = '192.168.5.35'
 user = 'root'
 password = '123456'
 dbname = 'hj21_backend'
@@ -19,23 +23,24 @@ cursor = db.cursor()
 vm_name = []
 vm_room = []
 #服务器清单
-host = ['172.25.1.5','172.25.1.4','172.25.1.3','172.25.1.2','172.25.1.1']
+host = ['192.168.5.30']
 #底层硬重启命令
 # cmd = 'xe vm-reboot force=true name-label='
 cmd = ['xe vm-start force=true name-label=','xe vm-reboot force=true name-label=']
 #
 
 # 获取教室里面的虚拟机信息
-query_vm = '''SELECT CONCAT(wtc.terminal_name,'-V'),room.classroom_name
+query_vm = '''SELECT CONCAT(wtc.terminal_name,'-T'),room.classroom_name
 from wtc_terminal wtc
-INNER  JOIN wtc_classroom room on wtc.classroom_id =room.id
+INNER  JOIN wtc_classroom room on wtc.classroom_id =room.id 
+WHERE wtc.remarks !=''
 '''
 try:
     cursor.execute(query_vm)
     result = cursor.fetchall()
     # 获取教室云桌面数量
     vm_count = len(result)
-    print 'A、B、C、D教室云桌面数量共{0}台'.format(vm_count)
+    print unicode('B901教室云桌面数量共{0}台'.format(vm_count),'utf-8')
     # print len(cursor.fetchall())
     # cursor.execute(query_vm)
     for vm_id in range(0,vm_count,1):
@@ -60,25 +65,66 @@ now_date = datetime.datetime.now().strftime('%H:%M')
 # cure_date = datetime.datetime.strptime(now_date,'%Y-%m-%d %H:%M:%S')
 # print now_date
 #自定义重启时间
-set_retime = ['12:58','22:00']
-
+set_retime = ['21:00']
+#自定义每隔5天重启一次
+global day
+day = 0
+count = 0
 while True:
     now_date = datetime.datetime.now().strftime('%H:%M:%S')
-    if datetime.datetime.now().strftime('%H:%M') in set_retime:
-
+    if datetime.datetime.now().strftime('%H:%M') in set_retime and (day%5) == 0:
+        day +=5
         #批量重启虚拟机
         for vm_id in range(0,vm_count,1):
             # cmd = 'xe vm-shutdown force=true name-label=%s' % (vm_name[vm_id])
             recmd ='xe vm-reboot force=true name-label=%s' %(vm_name[vm_id])
             scmd = 'xe vm-start force=true name-label=%s' % (vm_name[vm_id])
-            result=exec_commands(connect(host=host[0]), cmd=recmd)
-            if re.findall(r"halted",result,re.M|re.I):
-                exec_commands(connect(host=host[1]), cmd=scmd)
-                print '{0}的{1}正在开机，请等待注册\n'.format(vm_room[vm_id].encode('utf-8'), vm_name[vm_id].encode('utf-8'))
+            # ssh1 = connect(host=host[0])
+            result = exec_commands(connect(host=host[0]), cmd=recmd)
+            if re.findall(r"halted",result[1],re.M|re.I):
+                exec_commands(connect(host=host[0]), cmd=scmd)
+                print unicode('{0}的{1}正在开机，请等待注册\n'.format(vm_room[vm_id].encode('utf-8'), vm_name[vm_id].encode('utf-8')),'utf-8')
                 continue
-            print '现在正在重启{0}的{1}请等待注册\n'.format(vm_room[vm_id].encode('utf-8'),vm_name[vm_id].encode('utf-8'))
-            time.sleep(5)
+            print unicode('现在正在重启{0}的{1}请等待注册\n'.format(vm_room[vm_id].encode('utf-8'),vm_name[vm_id].encode('utf-8')),'utf-8')
+            time.sleep(60)
+        # ssh1.close()
     else:
-        print  '现在时间：{0}，还未到重启时间{1}，{2}，请等待\n'.format(now_date.encode('utf-8'),\
-                set_retime[0].encode('utf-8'), set_retime[1].encode('utf-8'))
-        time.sleep(5)
+        print  unicode('现在时间：{0}，还未到周五重启时间{1}，请等待\n'.format(now_date.encode('utf-8'),set_retime[0].encode('utf-8')),'utf-8')
+        # time.sleep(10)
+        # log = Logger(level='debug')
+
+        for vm_id in range(0,vm_count,1):
+            # cmd = 'xe vm-shutdown force=true name-label=%s' % (vm_name[vm_id])
+            # recmd ='xe vm-reboot force=true name-label=%s' %(vm_name[vm_id])
+            vm_status = 'xe vm-list  name-label=%s' %(vm_name[vm_id])
+            scmd = 'xe vm-start force=true name-label=%s' % (vm_name[vm_id])
+            count = count+vm_id
+            try:
+                if exec_commands(connect(host=host[0]),vm_status) :
+
+                    print count
+                    result = exec_commands(connect(host=host[0]),vm_status)
+                    print result
+                    # print re.findall(r"halted",result,re.M|re.I)
+                    if re.findall(r"halted",result[1],re.M|re.I):
+                        exec_commands(connect(host=host[0]), scmd)
+                        print unicode('{0}classroom的{1}正在开机......\n'.format(vm_room[vm_id].encode('utf-8'), vm_name[vm_id].encode('utf-8')),'utf-8')
+                # time.sleep(1)
+            except Exception as f:
+                print f,'exec_commands'
+
+                #Logger('error.log', level='error').logger.error(f)
+            finally:
+
+                # log.logger.debug('debug')
+                print count
+                logger.info(unicode("虚拟机名称："+str(vm_name[vm_id]),'utf-8'))
+                logger.info(unicode("虚拟机信息"+str(result[1])+str('\n'),'utf-8'))
+                # log.logger.warning('warning')
+                # log.logger.error('error')
+                # log.logger.critical('critical')
+                # time.sleep(1)
+
+
+
+
--- vm_tool.py
+++ vm_tool.py
@@ -3,20 +3,23 @@
 # import json
 
 import paramiko
+import logging,types
+from logging import handlers
 
 #ssh 方式连接底层xenserver服务器
 def connect(host):
     'this is use the paramiko connect the host,return conn'
     ssh = paramiko.SSHClient()
     username = 'root'
-    password = '1qaz@WSX'
+    password = 'xuanyuan@123'
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         #        ssh.connect(host,username='root',allow_agent=True,look_for_keys=True)
         ssh.connect(host, username=username, password=password, allow_agent=True)
         return ssh
-    except:
-        return None
+    except Exception,e:
+        print e,'ssh error'
+        return e
 
 
 # def command(args, outpath):
@@ -29,9 +32,14 @@ def connect(host):
 def exec_commands(conn, cmd):
     'this is use the conn to excute the cmd and return the results of excute the command'
     stdin, stdout, stderr = conn.exec_command(cmd)
-    results = stderr.read()
-    return results
+    results = []
+    if type(stderr) is not types.NoneType :
+        results.append(stderr.read())
 
+    if type(stdout) is not types.NoneType:
+        results.append(stdout.read())
+    conn.close()
+    return results
 
 # def excutor(host, outpath, args):
 #     conn = connect(host)
@@ -50,3 +58,33 @@ def exec_commands(conn, cmd):
 #     ftp.put(inpath,outpath)
 #     ftp.close()
 #     return outpath
+
+
+class Logger(object):
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }#日志级别关系映射
+
+    def __init__(self,filename,level='info',when='D',backCount=2,fmt='%(asctime)s - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)#设置日志格式
+        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
+        sh = logging.StreamHandler()#往屏幕上输出
+        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
+        #实例化TimedRotatingFileHandler
+        #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+        th.setFormatter(format_str)#设置文件里写入的格式
+        self.logger.addHandler(sh) #把对象加到logger里
+        self.logger.addHandler(th)
+
